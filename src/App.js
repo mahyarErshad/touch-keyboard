25c90edf8aad0a33e413ae079e8b83d99f76758a
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-16 11:15:00 +0330

    Imported TouchKeyboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import TouchKeyboard from "./Components/TouchKeyboard/TouchKeyboard";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <TouchKeyboard />
+    </div>
+  );
 }
 
 export default App;
