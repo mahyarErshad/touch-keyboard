5d2ed093931a0948d61322dba881edf50483d877
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-16 11:24:13 +0330

    Passed props to TouchKeyboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import TouchKeyboard from "./Components/TouchKeyboard/TouchKeyboard";
 
 function App() {
+  const [value, setValue] = useState("");
   return (
     <div className="container">
-      <TouchKeyboard />
+      <TouchKeyboard inputValue={value} setInputValue={setValue} passwordInput={false} length={9} valueReader={true} disableZero={true} />
     </div>
   );
 }
